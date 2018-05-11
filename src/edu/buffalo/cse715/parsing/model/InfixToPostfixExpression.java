2a8214bc8df0f0f4328eb5dea4d036536ca31084
Sandeep Kumar <skumar28@buffalo.edu>
2018-05-10 23:17:08 -0400

    Infix Expression to postfix expression

---
+++ src/edu/buffalo/cse715/parsing/model/InfixToPostfixExpression.java
@@ -0,0 +1,114 @@
+package edu.buffalo.cse715.parsing.model;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Stack;
+
+public class InfixToPostfixExpression {
+
+	// converts given infix expression to postfix expression.
+	public List<String> infixToPostfix(List<String> exp) {
+		// initializing empty String for result
+		List<String> result = new ArrayList<>();
+		Set<String> bracesSet = new HashSet<>();
+		bracesSet.add("[");
+		bracesSet.add("]");
+		bracesSet.add("(");
+		bracesSet.add(")");
+		// initializing empty stack
+		Stack<String> stack = new Stack<>();
+
+		for (int i = 0; i < exp.size(); ++i) {
+			String token = exp.get(i);
+			if (token.isEmpty())
+				continue;
+
+			// If the scanned character is an operand, add it to output.
+			boolean isOperator = false;
+			if (!Operators.OPERATOR_PRECEDENCE.containsKey(token) && !bracesSet.contains(token)) {
+				result.add(token);
+			}
+
+			// If the scanned character is an '[', push it to the stack.
+			else if (token.equals("["))
+				stack.push(token);
+
+			// If the scanned character is an ']', pop and output from the stack
+			// until an '(' is encountered.
+			else if (token.equals("]")) {
+				while (!stack.isEmpty() && !stack.peek().equals("[")) {
+					result.add(stack.pop());
+				}
+
+				if (!stack.isEmpty() && !stack.peek().equals("["))
+					return null;// "Invalid Expression"; // invalid expression
+				else
+					stack.pop();
+			} else if (token.equals("("))
+				stack.push(token);
+
+			// If the scanned character is an ')', pop and output from the stack
+			// until an '(' is encountered.
+			else if (token.equals(")")) {
+				while (!stack.isEmpty() && !stack.peek().equals("(")) {
+					result.add(stack.pop());
+				}
+
+				if (!stack.isEmpty() && !stack.peek().equals("("))
+					return null;// "Invalid Expression"; // invalid expression
+				else
+					stack.pop();
+			} else // an operator is encountered
+			{
+				while (!stack.isEmpty()) {
+					int precOp = Operators.OPERATOR_PRECEDENCE.get(token);
+					int stackPeekPrec = Operators.OPERATOR_PRECEDENCE.get(stack.peek()) == null ? -1
+							: Operators.OPERATOR_PRECEDENCE.get(stack.peek());
+					if (precOp < stackPeekPrec) {
+						result.add(stack.pop());
+
+					} else {
+						break;
+					}
+				}
+				stack.push(token);
+
+			}
+
+		}
+
+		// pop all the operators from the stack
+		while (!stack.isEmpty()) {
+			result.add(stack.pop());
+		}
+
+		return result;
+	}
+
+	// Driver method
+	public static void main(String[] args) {
+		// String exp = "a+b*(c^d-e)^(f+g*h)-i";
+		InfixToPostfixExpression inToPost = new InfixToPostfixExpression();
+		String exp = "G [w = 1 > r = 0]";
+		List<String> expList = new ArrayList<>();
+		expList.add("G");
+		expList.add("[");
+		expList.add("(");
+		expList.add("w");
+		expList.add("=");
+		expList.add("1");
+		expList.add(")");
+		expList.add("||");
+		expList.add("(");
+		expList.add("r");
+		expList.add("=");
+		expList.add("0");
+		expList.add(")");
+		expList.add("]");
+		// String exp = "a+b*[c^d-e]^[f+g*h]-i";
+		System.out.println(inToPost.infixToPostfix(expList));
+	}
+
+}
--- src/edu/buffalo/cse715/parsing/model/Operators.java
+++ src/edu/buffalo/cse715/parsing/model/Operators.java
@@ -20,8 +20,12 @@ public class Operators {
 		map.put("-", 11);
 		map.put("/", 12);
 		map.put("*", 12);
+		map.put("^", 13);
 		map.put("->", 16);
-		map.put("(", 15);
+		map.put("||", 15);
+		map.put("&&", 15);
+		//map.put("(", 15);
+		//map.put("[", 15);
 		// map.put("[", 1);
 		map.put("==", 8);
 		map.put("!=", 8);
@@ -30,10 +34,11 @@ public class Operators {
 		map.put("<", 9);
 		map.put(">", 9);
 		map.put("!", 14);
-		map.put("&&", 4);
-		map.put("||", 3);
-		map.put("A", 16);
-		map.put("G", 16);
+		//map.put("&&", 4);
+		//map.put("||", 3);
+		map.put("=", 17);
+		map.put("A", 18);
+		map.put("G", 18);
 		OPERATOR_PRECEDENCE = Collections.unmodifiableMap(map);
 	}
 

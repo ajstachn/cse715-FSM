52cc0d863b0ad52b06ddc4398e00413effff187b
DominiqueHickson <33875024+DominiqueHickson@users.noreply.github.com>
2018-05-24 15:56:15 -0400

    Update Division

--- src/edu/buffalo/cse715/parsing/expression/arithmetic/DivisionExpression.java
+++ src/edu/buffalo/cse715/parsing/expression/arithmetic/DivisionExpression.java
@@ -6,4 +6,10 @@ package edu.buffalo.cse715.parsing.expression.arithmetic;
  *
  */
 public class DivisionExpression extends ArithmeticExpression {
+  
+  public ValueExpression evaluate() {
+		Number lhs=((NumberValueExpression)this.getExpressionA()).getLiteral();
+		Number rhs=((NumberValueExpression)this.getExpressionB()).getLiteral();
+		return new NumberValueExpression((int)lhs / (int)rhs);
+	}
 }

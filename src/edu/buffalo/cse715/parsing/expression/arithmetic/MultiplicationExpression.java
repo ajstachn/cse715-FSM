4b2760c72a70b1c50cd8d1d12f92180e528edeb6
DominiqueHickson <33875024+DominiqueHickson@users.noreply.github.com>
2018-05-24 15:56:40 -0400

    Update Multiplication

--- src/edu/buffalo/cse715/parsing/expression/arithmetic/MultiplicationExpression.java
+++ src/edu/buffalo/cse715/parsing/expression/arithmetic/MultiplicationExpression.java
@@ -6,4 +6,10 @@ package edu.buffalo.cse715.parsing.expression.arithmetic;
  *
  */
 public class MultiplicationExpression extends ArithmeticExpression {
+  
+  public ValueExpression evaluate() {
+		Number lhs=((NumberValueExpression)this.getExpressionA()).getLiteral();
+		Number rhs=((NumberValueExpression)this.getExpressionB()).getLiteral();
+		return new NumberValueExpression((int)lhs*(int)rhs);
+	}
 }

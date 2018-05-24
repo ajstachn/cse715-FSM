22a0a2e5deb2dc0cb21ded3878bbeae36f74103f
DominiqueHickson <33875024+DominiqueHickson@users.noreply.github.com>
2018-05-24 15:55:31 -0400

    Update addition

--- src/edu/buffalo/cse715/parsing/expression/arithmetic/AdditionExpression.java
+++ src/edu/buffalo/cse715/parsing/expression/arithmetic/AdditionExpression.java
@@ -7,4 +7,10 @@ package edu.buffalo.cse715.parsing.expression.arithmetic;
  */
 public class AdditionExpression extends ArithmeticExpression {
 
+  public ValueExpression evaluate() {
+		Number lhs=((NumberValueExpression)this.getExpressionA()).getLiteral();
+		Number rhs=((NumberValueExpression)this.getExpressionB()).getLiteral();
+		return new NumberValueExpression((int)lhs+(int)rhs);
+	}
+  
 }
